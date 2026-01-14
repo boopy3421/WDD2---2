01ca90b465e1824b54ec413615d7363502fcdaa1
CIITSHSDTP00\CIIT <student@ciit.edu.ph>
2026-01-14 13:31:40 +0800

    WEEK 2 updated back end

--- activities/backend/index.js
+++ activities/backend/index.js
@@ -1,8 +1,35 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-app.listen(port, () => {
+//get -> display name, var name ="Moreno"
+
+//post
+
+app.use(express.json());
+
+app.get("/getName", (req, res) => {
+  var name = "John Christian Moreno";
+  res.status(200).json(name);
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (username == "John Christian Moreno" && password == "123321") {
+    res.status(200).json({
+      message: "LOGIN SUCCESSFUL",
+      status: "success",
+    });
+  } else {
+    res.status(403).json({
+      message: "Invalid Username or Password",
+      status: "Failed",
+    });
+  }
+});
+
+app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
 });
