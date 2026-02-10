8e236909e94aba1030447c18257381ce67b2fb4f
Ma. Therese Andrea P. Carrillo <mathereseandreacarrillo0914@gmail.com>
2026-02-10 19:46:19 +0800

    updated back end scaffolding

--- activities/backend/index.js
+++ activities/backend/index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
@@ -15,21 +18,50 @@ app.get("/getName", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  const { username, password } = req.body;
+  const { username, password } = req.body; // destructuring assignment, JSON object
 
-  if (username == "John Christian Moreno" && password == "123321") {
-    res.status(200).json({
-      message: "LOGIN SUCCESSFUL",
-      status: "success",
-    });
-  } else {
-    res.status(403).json({
-      message: "Invalid Username or Password",
-      status: "Failed",
-    });
+  // basic validation
+  if (!username || !password) {
+    return res.status(400).send("username and password fields are required") // 400 bad request
   }
+  // simulate user authentication
+  const user = data.find(
+    (u) => u.username === username && u.password === password
+  );
+
+  // log the registered users
+  if (user) {
+    res.send('user ${username} logged in successfully');
+  } else {
+    res.status(401).send("Invalid username or password"); // 401 unauthorized 
+  }
+});
+
+app.post("/register", (req, res) => {
+  const { username, name, password, role } = req.body;
+  // BASIC VALIDATION
+  if (!username || !password || !role) {
+    return res
+      .status(400)
+      .send("username, password and role fields are required"); // 400 bad request
+  }
+  // store user data in memory (DEMO)
+  data.push({ username, name, password, role });
+
+  // Log the registered users
+  console.log("Registered Users:", data);
+
+  // simulate user registration
+  res.send('User ${username} registered successfully');
+});
+
+app.post("/logout", (req, res) => {
+  // Simulate user logout
+  res.send("User logged out successfully");
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
+  // log the registered users
+  console.log("Registered Users:", data);
 });
